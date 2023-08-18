5e9dad282d1024532d2dfe3f59c5df6634257256
emrec <emrec@DESKTOP-KRPHMD7>
2023-08-18 19:35:17 +0300

    ⠀

--- personel/src/main/java/com/ekran/personel/entity/Personel.java
+++ personel/src/main/java/com/ekran/personel/entity/Personel.java
@@ -1,61 +1,40 @@
 package com.ekran.personel.entity;
 
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
 
 @Entity
 @Table(name = "personel")
+@Data
 public class Personel {	
-	
 	@Id
 	private Long id;
 	private String firstName;
 	private String lastName;
 	private String address;
 	private Integer age;
+
+	@Column(name = "created_at", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+	@CreationTimestamp
+    private Date createdAt;
+	
+	@Column(name = "last_updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+	@UpdateTimestamp
+    private Date lastUpdatedAt;
 	
 	public Personel() {
 		
-	}
-	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
 	}
 }
